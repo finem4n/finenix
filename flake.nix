832f3d03aaabb468846bf5b521970864fefb8d40
Konrad Marciniak <marciniakkonrad@protonmail.com>
2026-01-10 23:31:14 +0100

    fix(flake.nix): secureboot import

--- flake.nix
+++ flake.nix
@@ -12,7 +12,9 @@
     nixosConfigurations.hp_desktop_pro_g2 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        # TODO: create common modules
         disko.nixosModules.disko
+        ./wihajsters/secureboot-lanzaboote
 	./hosts/x86_64-linux/hp_desktop_pro_g2
       ];
     };
